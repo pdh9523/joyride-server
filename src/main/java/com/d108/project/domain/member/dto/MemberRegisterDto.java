4e13238bb05673646295fb5a95568e6f30ce5d5d
sihyunjojo <si4018@naver.com>
2024-09-05 20:07:08 +0900

    refactor: 생성자 추가

--- src/main/java/com/d108/project/domain/member/dto/MemberRegisterDto.java
+++ src/main/java/com/d108/project/domain/member/dto/MemberRegisterDto.java
@@ -1,10 +1,12 @@
 package com.d108.project.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class MemberRegisterDto {
     private String nickname;
     private String username;
--- src/main/java/com/d108/project/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/d108/project/domain/member/dto/MemberResponseDto.java
@@ -1,10 +1,12 @@
 package com.d108.project.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class MemberResponseDto {
     private Integer id;
     private String username;
