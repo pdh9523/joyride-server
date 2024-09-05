ebec641fde43e715f4384dab5bb4a714bdb9f789
조시현 <si4018@naver.com>
2024-09-05 18:36:47 +0900

    refactor: 타입 변경에 따른 코드 변경

--- src/main/java/com/d108/project/domain/member/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.d108.project.domain.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
