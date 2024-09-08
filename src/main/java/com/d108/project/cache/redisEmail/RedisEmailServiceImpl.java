96e51ab296e9a5e0d6667b7b195f1c20c286d251
박동현 <jinee9523@gmail.com>
2024-09-08 17:23:44 +0900

    Feat: spring mail 설정

--- src/main/java/com/d108/project/cache/redisEmail/RedisEmailServiceImpl.java
+++ src/main/java/com/d108/project/cache/redisEmail/RedisEmailServiceImpl.java
@@ -3,7 +3,9 @@ package com.d108.project.cache.redisEmail;
 import com.d108.project.cache.redis.RedisUtil;
 import com.d108.project.cache.redisEmail.entity.RedisEmail;
 import com.d108.project.cache.redisEmail.repository.RedisEmailRepository;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -13,14 +15,20 @@ import java.security.SecureRandom;
 public class RedisEmailServiceImpl implements RedisEmailService {
 
     private final RedisUtil redisUtil;
+    private final JavaMailSender mailSender;
     private static final SecureRandom random = new SecureRandom();
 
     // 임의의 6자리 숫자 이메일 인증 코드 생성
     // 이건 private 로 설정하고, 이메일 전송과 검증만 public 으로 하면 될 듯
-    private String makeAuthCode(String email) {
+    private String createAuthCode(String email) {
         String authCode = String.format("%06d", random.nextInt(900000) + 100000);
         redisUtil.setDataExpire(email, authCode, 60*5);
         return authCode;
+    }
+    // TODO: 이메일 생성
+    private void createEmail() {
+        MimeMessage message = mailSender.createMimeMessage();
+
     }
 
     // 이메일과 인증코드를 검증
@@ -40,5 +48,5 @@ public class RedisEmailServiceImpl implements RedisEmailService {
     public void sendEmail(String email) {
 
     }
-    // TODO: 이메일 생성
+
 }
