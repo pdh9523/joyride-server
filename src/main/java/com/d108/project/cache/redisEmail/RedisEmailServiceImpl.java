1f68eb5761b90c6ddfc52c52b00096c593ff543c
박동현 <jinee9523@gmail.com>
2024-09-09 09:51:50 +0900

    Comment: 레디스 주석 추가

--- src/main/java/com/d108/project/cache/redisEmail/RedisEmailServiceImpl.java
+++ src/main/java/com/d108/project/cache/redisEmail/RedisEmailServiceImpl.java
@@ -22,27 +22,33 @@ public class RedisEmailServiceImpl implements RedisEmailService {
     private final RedisUtil redisUtil;
     private final JavaMailSender mailSender;
     private static final SecureRandom random = new SecureRandom();
+    
 
-    // 임의의 6자리 숫자 이메일 인증 코드 생성
-    // 이건 private 로 설정하고, 이메일 전송과 검증만 public 으로 하면 될 듯
+    /**
+     * 이메일 인증을 위한 임의의 6자리 숫자 인증 코드를 생성하고, 레디스에 저장
+     * @param email 인증 요청한 이메일 
+     * @return 인증코드
+     */
+    // 얘는 private로 설정
     private String createAuthCode(String email) {
         String authCode = String.format("%06d", random.nextInt(900000) + 100000);
-        redisUtil.setDataExpire(email, authCode, 60*5);
+        redisUtil.setDataExpire("auth: "+email, authCode, 60*5);    // 60sec*5min
         return authCode;
     }
 
-    // 이메일과 인증코드를 검증
+    // 인증코드 검증
     @Override
     public Boolean checkAuthCode(String email, String authCode) {
         // email의 키값으로 저장된 코드와 입력한 인증코드와 같은지 다른지 검증
-        if (authCode.equals(redisUtil.getData(email))) {
+        if (authCode.equals(redisUtil.getData("auth: "+email))) {
             // 코드가 일치하면 레디스에서 즉시 삭제
-            redisUtil.deleteData(email);
+            redisUtil.deleteData("auth: "+email);
             return true;
         }
         return false;
     }
 
+    // 인증 메일 발송
     @Override
     public void sendEmail(String email) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
